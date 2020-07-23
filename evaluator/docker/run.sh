02542700558b7aee72183a2642b4ccc3c51d09a4
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2020-07-23 17:21:44 +0300

    modify docker script

--- evaluator/docker/run.sh
+++ evaluator/docker/run.sh
@@ -2,16 +2,33 @@
 
 touch /kobe/run.prop
 
-if [ "$TIMEOUT" ]; then
-    echo timeout = "$TIMEOUT" >> /kobe/run.prop
+if [ "$TIMEOUT" ]
+then
+  echo timeout = "$TIMEOUT" >> /kobe/run.prop
 fi
 
-if [ "$EVAL_RUNS" ]; then
-    echo evalRuns = "$EVAL_RUNS" >> /kobe/run.prop
+if [ "$EVAL_RUNS" ]
+then
+  echo evalRuns = "$EVAL_RUNS" >> /kobe/run.prop
 fi
 
-if [ "$EXPERIMENT" ]; then
-    echo experimentName = "$EXPERIMENT" >> /kobe/run.prop
+if [ "$EXPERIMENT" ]
+then
+  echo experimentName = "$EXPERIMENT" >> /kobe/run.prop
+fi
+
+if [ "$TEMPLATE" ]
+then
+  mkdir /tmp/orig
+  mv /queries/* /tmp/orig
+  
+  for Q in `ls /tmp/orig`
+  do
+    for T in $TEMPLATE
+    do
+      sed 's/\$\#\$/'$T'/g' "/tmp/orig/"$Q > "/queries/"$Q"-"$T
+    done
+  done
 fi
 
 cd /kobe/
